832f10cf3296da2ffe4647651bb626842a782264
practicalClerk <ayushsinha40863@gmail.com>
2026-02-16 15:52:35 +0530

    Remove titanwork_v1.2 folder from repository

--- titanwork_v1.2/Backend/static/component/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="mt-10 text-gray-600 text-sm">
-      2025 AI Fraud Detection | Project In Progress
-    </footer>
-  );
-}
